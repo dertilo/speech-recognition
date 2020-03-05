2e982de012abecb25e52b359dc373f6d3c8afd67
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2020-03-05 13:46:55 +0100

    data augmentation

--- data_related/data_augmentation/signal_augment.py
+++ data_related/data_augmentation/signal_augment.py
@@ -33,6 +33,35 @@ def build_sox_noise(audio_file, lowpass_cutoff=1, noise_gain=-4):
     return sox_params
 
 
+def build_varying_amplitude_factor(audio_file, lowpass_cutoff=1, ac_gain=-9):
+    ac = "sox {} -p synth whitenoise lowpass {} gain -n {}".format(
+        audio_file, lowpass_cutoff, ac_gain
+    )
+    dc = "sox {} -p gain -90 dcshift 0.5".format(audio_file)
+    return "sox -m <({}) <({}) -p".format(ac, dc)
+
+
+def multiply_signals(signal_a, signal_b):
+    return ("sox -T <({signal_a}) <({signal_b}) -p").format(
+        signal_a=signal_a, signal_b=signal_b,
+    )
+
+
+def build_sox_interference(
+    interfere_file, interfere_gain=-12, lowpass_cutoff=1, ac_gain=-6
+):
+    factor = build_varying_amplitude_factor(interfere_file, lowpass_cutoff, ac_gain)
+    signal = "sox {} -p gain {} reverse".format(interfere_file, interfere_gain)
+    return multiply_signals(factor, signal)
+
+
+def add_signals_trim_to_len(original, signals):
+    signals_to_add = " ".join(["<(%s)" % s for s in signals])
+    sox_cmd = "sox -m {signals} -p trim 0 $(soxi -D {original})".format(
+        signals=signals_to_add, original=original
+    )
+    return sox_cmd
+
 if __name__ == "__main__":
     """
     play original.wav tempo 1.4 gain -9 pitch -100 reverb 50 80 100 10 0 0
@@ -40,13 +69,13 @@ if __name__ == "__main__":
 
     original = "/tmp/original.wav"
     augmented = "/tmp/augmented.wav"
+    interfere = "/tmp/interfere.wav"
 
     signal = build_sox_distortions(original, gain=-4, tempo=1.2, pitch=-200, reverb=50)
-    noise = build_sox_noise(original,noise_gain=0)
-    # subprocess.call(["bash", "-c", sox_pipe+' > '+augmented])
+    noise = build_sox_noise(original, noise_gain=0)
+    interf = build_sox_interference(interfere)
 
-    sox_cmd = "sox -m <({noise}) <({signal}) {augmented}".format(
-        noise=noise, signal=signal, augmented=augmented
-    )
-
+    sox_pipe = add_signals_trim_to_len(original,[signal,noise,interf])
+    sox_cmd = sox_pipe +' > '+augmented
+    print(sox_cmd)
     subprocess.call(["bash", "-c", sox_cmd])
