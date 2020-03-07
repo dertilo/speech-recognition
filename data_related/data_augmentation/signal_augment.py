e338391d461184ca95eb43eb6a781f0ef5cf12ff
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2020-03-07 11:54:40 +0100

    16 bit

--- data_related/data_augmentation/signal_augment.py
+++ data_related/data_augmentation/signal_augment.py
@@ -70,7 +70,7 @@ def build_sox_interference(
 
 def add_signals_trim_to_len(original, signals, augmented):
     signals_to_add = " ".join(["<(%s)" % s for s in signals])
-    sox_cmd = "sox -m {signals} {augmented} trim 0 $(soxi -D {original})".format(
+    sox_cmd = "sox -m {signals} -b 16 {augmented} trim 0 $(soxi -D {original})".format(
         signals=signals_to_add, original=original, augmented=augmented
     )
     return sox_cmd
