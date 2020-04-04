a13d461520903a5ec9a1fca2bca3f7419f173542
Tilo Himmelsbach <dertilo@gmail.com>
2020-04-04 20:15:44 +0200

    audio_util.py

---
+++ audio_util.py
@@ -0,0 +1,18 @@
+import numpy
+import torchaudio
+
+
+def load_audio(audio_file: str, target_rate=16_000) -> numpy.ndarray:
+    si, _ = torchaudio.info(audio_file)
+    normalize_denominator = 1 << si.precision
+    sound, sample_rate = torchaudio.load(
+        audio_file, normalization=normalize_denominator
+    )
+    if sample_rate != target_rate:
+        resampler = torchaudio.transforms.Resample(
+            orig_freq=sample_rate, new_freq=target_rate
+        )
+        sound = resampler(sound)
+
+    y = sound.squeeze().numpy()
+    return y
