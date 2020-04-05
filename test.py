60f2381b6bc54a71fe2991393cafa01d5c2a91fc
Tilo Himmelsbach <dertilo@gmail.com>
2020-04-05 14:59:19 +0200

    main in test is todo

--- test.py
+++ test.py
@@ -5,9 +5,7 @@ import numpy as np
 import torch
 from tqdm import tqdm
 
-from data_related.data_loader import SpectrogramDataset, AudioDataLoader
-from decoder import GreedyDecoder
-from opts import add_decoder_args, add_inference_args
+from opts_removeit import add_decoder_args, add_inference_args
 from utils import load_model, reduce_tensor, calc_loss
 
 parser = argparse.ArgumentParser(description="DeepSpeech transcription")
@@ -110,55 +108,56 @@ def evaluate(
     # print("avg valid loss %0.2f" % avg_loss)
     return wer * 100, cer * 100, avg_loss, output_data
 
+#TODO(tilo)
 
-if __name__ == "__main__":
-    args = parser.parse_args()
-    torch.set_grad_enabled(False)
-    device = torch.device("cuda" if args.cuda else "cpu")
-    model = load_model(device, args.model_path, args.half)
-
-    if args.decoder == "beam":
-        from decoder import BeamCTCDecoder
-
-        decoder = BeamCTCDecoder(
-            model.labels,
-            lm_path=args.lm_path,
-            alpha=args.alpha,
-            beta=args.beta,
-            cutoff_top_n=args.cutoff_top_n,
-            cutoff_prob=args.cutoff_prob,
-            beam_width=args.beam_width,
-            num_processes=args.lm_workers,
-        )
-    elif args.decoder == "greedy":
-        decoder = GreedyDecoder(model.labels, blank_index=model.labels.index("_"))
-    else:
-        decoder = None
-    target_decoder = GreedyDecoder(model.labels, blank_index=model.labels.index("_"))
-    test_dataset = SpectrogramDataset(
-        audio_conf=model.audio_conf,
-        manifest_filepath=args.test_manifest,
-        labels=model.labels,
-        normalize=True,
-    )
-    test_loader = AudioDataLoader(
-        test_dataset, batch_size=args.batch_size, num_workers=args.num_workers
-    )
-    wer, cer, avg_loss, output_data = evaluate(
-        test_loader=test_loader,
-        device=device,
-        model=model,
-        decoder=decoder,
-        target_decoder=target_decoder,
-        save_output=args.save_output,
-        verbose=args.verbose,
-        half=args.half,
-    )
-
-    print(
-        "Test Summary \t"
-        "Average WER {wer:.3f}\t"
-        "Average CER {cer:.3f}\t".format(wer=wer, cer=cer)
-    )
-    if args.save_output is not None:
-        np.save(args.save_output, output_data)
+# if __name__ == "__main__":
+#     args = parser.parse_args()
+#     torch.set_grad_enabled(False)
+#     device = torch.device("cuda" if args.cuda else "cpu")
+#     model = load_model(device, args.model_path, args.half)
+#
+#     if args.decoder == "beam":
+#         from decoder import BeamCTCDecoder
+#
+#         decoder = BeamCTCDecoder(
+#             model.vocab_size,
+#             lm_path=args.lm_path,
+#             alpha=args.alpha,
+#             beta=args.beta,
+#             cutoff_top_n=args.cutoff_top_n,
+#             cutoff_prob=args.cutoff_prob,
+#             beam_width=args.beam_width,
+#             num_processes=args.lm_workers,
+#         )
+#     elif args.decoder == "greedy":
+#         decoder = GreedyDecoder(model.vocab_size, blank_index=model.vocab_size.index("_"))
+#     else:
+#         decoder = None
+#     target_decoder = GreedyDecoder(model.vocab_size, blank_index=model.vocab_size.index("_"))
+#     test_dataset = CharSTTDataset(
+#         audio_conf=model.audio_conf,
+#         manifest_filepath=args.test_manifest,
+#         labels=model.vocab_size,
+#         normalize=True,
+#     )
+#     test_loader = AudioDataLoader(
+#         test_dataset, batch_size=args.batch_size, num_workers=args.num_workers
+#     )
+#     wer, cer, avg_loss, output_data = evaluate(
+#         test_loader=test_loader,
+#         device=device,
+#         model=model,
+#         decoder=decoder,
+#         target_decoder=target_decoder,
+#         save_output=args.save_output,
+#         verbose=args.verbose,
+#         half=args.half,
+#     )
+#
+#     print(
+#         "Test Summary \t"
+#         "Average WER {wer:.3f}\t"
+#         "Average CER {cer:.3f}\t".format(wer=wer, cer=cer)
+#     )
+#     if args.save_output is not None:
+#         np.save(args.save_output, output_data)
