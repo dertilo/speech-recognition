8a3f1f9bdd2b49e60c3f299b4fabbb10fb9647e7
Tilo Himmelsbach <dertilo@gmail.com>
2020-05-21 17:06:40 +0200

    pad targets

--- lightning/lightning_model.py
+++ lightning/lightning_model.py
@@ -3,14 +3,17 @@ from typing import NamedTuple
 
 import pytorch_lightning as pl
 import torch
+from pytorch_lightning import Trainer
 from test_tube import HyperOptArgumentParser
 from collections import OrderedDict
 import torch as t
 import numpy as np
 import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data.dataloader import DataLoader
+
 from data_related.audio_feature_extraction import AudioFeaturesConfig
 from data_related.char_stt_dataset import DataConfig, CharSTTDataset
-from data_related.data_loader import AudioDataLoader
 from data_related.librispeech import LIBRI_VOCAB, build_librispeech_corpus
 from model import DeepSpeech
 from utils import BLANK_SYMBOL
@@ -58,8 +61,8 @@ class LitSTTModel(pl.LightningModule):
         ctc_loss = F.ctc_loss(
             prob.transpose(0, 1),
             targets,
-            input_sizes,
             output_sizes,
+            target_sizes,
             blank=BLANK_INDEX,
             zero_infinity=True,
         )
@@ -75,7 +78,8 @@ class LitSTTModel(pl.LightningModule):
     def train_dataloader(self):
         dataloader = AudioDataLoader(
             train_dataset,
-            num_workers=1,
+            num_workers=0,
+            batch_size=3
             # batch_sampler=train_sampler # TODO: is lightning providing this?
         )
         return dataloader
@@ -95,6 +99,41 @@ class LitSTTModel(pl.LightningModule):
         parser.add_argument("--val_batch_size", default=20, type=int)
         return parser
 
+def _collate_fn(batch):
+    def func(p):
+        return p[0].size(1)
+
+    batch = sorted(batch, key=lambda sample: sample[0].size(1), reverse=True)
+    longest_sample = max(batch, key=func)[0]
+    freq_size = longest_sample.size(0)
+    minibatch_size = len(batch)
+    max_seqlength = longest_sample.size(1)
+    inputs = torch.zeros(minibatch_size, 1, freq_size, max_seqlength)
+    input_len_proportion = torch.FloatTensor(minibatch_size)
+    target_sizes = torch.IntTensor(minibatch_size)
+    targets = []
+    for x in range(minibatch_size):
+        sample = batch[x]
+        tensor = sample[0]
+        target = sample[1]
+        seq_length = tensor.size(1)
+        inputs[x][0].narrow(1, 0, seq_length).copy_(tensor)
+        input_len_proportion[x] = seq_length / float(max_seqlength)
+        target_sizes[x] = len(target)
+        targets.append(torch.IntTensor(target))
+
+    padded_target = pad_sequence(targets, batch_first=True)
+
+    return inputs, padded_target, input_len_proportion, target_sizes
+
+
+class AudioDataLoader(DataLoader):
+    def __init__(self, *args, **kwargs):
+        """
+        Creates a data loader for AudioDatasets.
+        """
+        super().__init__(*args, **kwargs)
+        self.collate_fn = _collate_fn
 
 if __name__ == "__main__":
 
@@ -118,3 +157,10 @@ if __name__ == "__main__":
             vocab_size=vocab_size,
         )
     )
+
+    trainer = Trainer(
+        logger=False,
+        checkpoint_callback=False,
+        max_epochs=3
+    )
+    trainer.fit(litmodel)
