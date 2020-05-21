56b916ea4a66f20b292cb05d9226fd804ebabdaf
Tilo Himmelsbach <dertilo@gmail.com>
2020-05-21 16:37:51 +0200

    cleaning

--- lightning/lightning_model.py
+++ lightning/lightning_model.py
@@ -2,6 +2,7 @@ import os
 from typing import NamedTuple
 
 import pytorch_lightning as pl
+import torch
 from test_tube import HyperOptArgumentParser
 from collections import OrderedDict
 import torch as t
@@ -9,6 +10,7 @@ import numpy as np
 import torch.nn.functional as F
 from data_related.audio_feature_extraction import AudioFeaturesConfig
 from data_related.char_stt_dataset import DataConfig, CharSTTDataset
+from data_related.data_loader import AudioDataLoader
 from data_related.librispeech import LIBRI_VOCAB, build_librispeech_corpus
 from model import DeepSpeech
 from utils import BLANK_SYMBOL
@@ -54,115 +56,43 @@ class LitSTTModel(pl.LightningModule):
 
         prob = F.log_softmax(out, -1)
         ctc_loss = F.ctc_loss(
-            prob.transpose(0, 1), targets, input_sizes, output_sizes, blank=BLANK_INDEX, zero_infinity=True)
+            prob.transpose(0, 1),
+            targets,
+            input_sizes,
+            output_sizes,
+            blank=BLANK_INDEX,
+            zero_infinity=True,
+        )
 
         loss = ctc_loss / out.size(0)  # average the loss by minibatch
 
-        tqdm_dict = {
-            "train-loss": loss,
-        }
+        tqdm_dict = {"train-loss": loss}
         output = OrderedDict(
-            {
-                "loss": loss,
-                "progress_bar": tqdm_dict,
-                "log": tqdm_dict,
-            }
+            {"loss": loss, "progress_bar": tqdm_dict, "log": tqdm_dict,}
         )
         return output
 
     def train_dataloader(self):
-        # dataloader = build_multi_dataloader(
-        #     record_root='data/tfrecords/{}.tfrecord',
-        #     index_root='data/tfrecord_index/{}.index',
-        #     data_name_list=[
-        #         # 'magic_data_train_562694',
-        #         'data_aishell_train_117346',
-        #         # 'c_500_train_549679',
-        #         # 'ce_200_161001'
-        #     ],
-        #     batch_size=self.hparams.train_batch_size,
-        #     num_workers=self.hparams.train_loader_num_workers
-        # )
-        dataloader = build_raw_data_loader(
-            [
-                # 'data/filterd_manifest/ce_200.csv',
-                data_path + "/lightning_manifests/train-clean-100.csv",
-                data_path + "/lightning_manifests/train-clean-360.csv",
-                data_path + "/lightning_manifests/train-other-500.csv",
-                # 'data/filterd_manifest/c_500_train.csv',
-                # 'data/filterd_manifest/aidatatang_200zh_train.csv',
-                # 'data/filterd_manifest/data_aishell_train.csv',
-                # 'data/filterd_manifest/AISHELL-2.csv',
-                # 'data/filterd_manifest/magic_data_train.csv',
-                # 'data/manifest/libri_100.csv',
-                # 'data/manifest/libri_360.csv',
-                # 'data/manifest/libri_500.csv'
-            ],
-            vocab_path=self.hparams.vocab_path,
-            batch_size=self.hparams.train_batch_size,
-            num_workers=self.hparams.train_loader_num_workers,
-            speed_perturb=True,
-            max_duration=15,
+        dataloader = AudioDataLoader(
+            train_dataset,
+            num_workers=1,
+            # batch_sampler=train_sampler # TODO: is lightning providing this?
         )
         return dataloader
 
-    def optimizer_step(
-        self, epoch_nb, batch_nb, optimizer, optimizer_i, second_order_closure=None
-    ):
-        lr = self.hparams.factor * (
-            (self.hparams.model_size ** -0.5)
-            * min(
-                (self.global_step + 1) ** -0.5,
-                (self.global_step + 1) * (self.hparams.warm_up_step ** -1.5),
-            )
-        )
-        self.lr = lr
-        for pg in optimizer.param_groups:
-            pg["lr"] = lr
-        optimizer.step()
-        optimizer.zero_grad()
-
     def configure_optimizers(self):
-        optimizer = AdamW(self.parameters(), lr=self.hparams.lr, betas=(0.9, 0.997))
-        optimizer = Lookahead(optimizer)
+        optimizer = torch.optim.Adam(
+            self.model.parameters(), lr=self.lr, weight_decay=1e-5
+        )
         return optimizer
 
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = HyperOptArgumentParser(parents=[parent_parser])
-        parser.add_argument("--num_freq_mask", default=2, type=int)
-        parser.add_argument("--num_time_mask", default=2, type=int)
-        parser.add_argument("--freq_mask_length", default=30, type=int)
-        parser.add_argument("--time_mask_length", default=20, type=int)
-        parser.add_argument("--feature_dim", default=400, type=int)
-        parser.add_argument("--model_size", default=512, type=int)
-        parser.add_argument("--feed_forward_size", default=2048, type=int)
-        parser.add_argument("--hidden_size", default=64, type=int)
-        parser.add_argument("--dropout", default=0.1, type=float)
-        parser.add_argument("--num_head", default=8, type=int)
         parser.add_argument("--num_encoder_layer", default=6, type=int)
-        parser.add_argument("--num_decoder_layer", default=6, type=int)
-        parser.add_argument(
-            "--vocab_path",
-            default=data_path + "/lightning_corpus/librispeech.model",
-            type=str,
-        )
-        parser.add_argument("--max_feature_length", default=1024, type=int)
-        parser.add_argument("--max_token_length", default=50, type=int)
-        parser.add_argument("--share_weight", default=True, type=bool)
-        parser.add_argument("--loss_lambda", default=0.8, type=float)
-        parser.add_argument("--smoothing", default=0.1, type=float)
-
         parser.add_argument("--lr", default=3e-4, type=float)
-        parser.add_argument("--warm_up_step", default=16000, type=int)
-        parser.add_argument("--factor", default=1, type=int)
-        parser.add_argument("--enable_spec_augment", default=True, type=bool)
-
         parser.add_argument("--train_batch_size", default=20, type=int)
-        parser.add_argument("--train_loader_num_workers", default=8, type=int)
         parser.add_argument("--val_batch_size", default=20, type=int)
-        parser.add_argument("--val_loader_num_workers", default=8, type=int)
-
         return parser
 
 
