6a1d028c118906fd7e59aa0f21b41959e662e139
Tilo Himmelsbach <dertilo@gmail.com>
2020-05-22 10:41:27 +0200

    too many changes to lightning_model.py

--- lightning/lightning_model.py
+++ lightning/lightning_model.py
@@ -1,5 +1,5 @@
 import os
-from typing import NamedTuple
+from typing import NamedTuple, Dict, Union, List
 
 import pytorch_lightning as pl
 import torch
@@ -9,13 +9,18 @@ from collections import OrderedDict
 import torch as t
 import numpy as np
 import torch.nn.functional as F
+from torch import Tensor
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data.dataloader import DataLoader
 
 from data_related.audio_feature_extraction import AudioFeaturesConfig
 from data_related.char_stt_dataset import DataConfig, CharSTTDataset
 from data_related.librispeech import LIBRI_VOCAB, build_librispeech_corpus
+from decoder import Decoder, convert_to_strings
+from evaluation import validation_step, calc_errors
+from metrics_calculation import calc_num_word_errors, calc_num_char_erros
 from model import DeepSpeech
+from transcribing.transcribe_util import build_decoder
 from utils import BLANK_SYMBOL
 
 
@@ -25,6 +30,8 @@ class Params(NamedTuple):
     audio_feature_dim: int
     vocab_size: int
     bidirectional: bool = True
+    num_workers: int = 2
+    batch_size: int = 4
 
 
 class LitSTTModel(pl.LightningModule):
@@ -39,6 +46,9 @@ class LitSTTModel(pl.LightningModule):
             input_feature_dim=hparams.audio_feature_dim,
             bidirectional=hparams.bidirectional,
         )
+        self.char2idx = dict([(l, i) for i, l in enumerate(LIBRI_VOCAB)])
+        self.idx2char = {v: k for k, v in self.char2idx.items()}
+        self.decoder = build_decoder(self.char2idx, use_beam_decoder=False)
 
     def forward(self, inputs, input_sizes):
         return self.model(inputs, input_sizes)
@@ -57,32 +67,85 @@ class LitSTTModel(pl.LightningModule):
 
         out, output_sizes = self(inputs, input_sizes)
 
-        prob = F.log_softmax(out, -1)
-        ctc_loss = F.ctc_loss(
-            prob.transpose(0, 1),
-            targets,
-            output_sizes,
-            target_sizes,
-            blank=BLANK_INDEX,
-            zero_infinity=True,
-        )
-
-        loss = ctc_loss / out.size(0)  # average the loss by minibatch
-
+        loss = calc_loss(out, output_sizes, targets, target_sizes)
         tqdm_dict = {"train-loss": loss}
         output = OrderedDict(
             {"loss": loss, "progress_bar": tqdm_dict, "log": tqdm_dict,}
         )
         return output
 
-    def train_dataloader(self):
+    def train_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        dataset = build_dataset(
+            # "train", ["train-clean-100", "train-clean-360", "train-other-500"]
+            "debug", ["dev-clean"]
+        )
         dataloader = AudioDataLoader(
-            train_dataset,
-            num_workers=0,
-            batch_size=3
+            dataset,
+            num_workers=self.hparams.num_workers,
+            batch_size=self.hparams.batch_size
             # batch_sampler=train_sampler # TODO: is lightning providing this?
         )
         return dataloader
+
+    def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        dataset = build_dataset("eval", ["dev-clean", "dev-other"])
+        dataloader = AudioDataLoader(
+            dataset,
+            num_workers=self.hparams.num_workers,
+            batch_size=self.hparams.batch_size
+            # batch_sampler=train_sampler # TODO: is lightning providing this?
+        )
+        return dataloader
+
+    def _calc_error(self, targets, decoded_output):
+        target_strings = convert_to_strings(
+            self.idx2char, self.char2idx[BLANK_SYMBOL], targets
+        )
+        total_wer, total_cer, num_chars, num_tokens = 0, 0, 0, 0
+        for x in range(len(target_strings)):
+            transcript, reference = decoded_output[x][0], target_strings[x][0]
+            total_wer += calc_num_word_errors(transcript, reference)
+            total_cer += calc_num_char_erros(transcript, reference)
+            num_tokens += len(reference.split())
+            num_chars += len(reference.replace(" ", ""))
+        return total_wer, total_cer, num_tokens, num_chars
+
+    def validation_step(self, batch: tuple, batch_nb: int, *args, **kwargs) -> Dict:
+        inputs, targets, input_percentages, target_sizes = batch
+
+        decoded_output, out, output_sizes = transcribe_batch(
+            self.decoder, input_percentages, inputs, self.model
+        )
+        loss_value = calc_loss(out, output_sizes, target_sizes, targets).item()
+        total_wer, total_cer, num_tokens, num_chars = self._calc_error(
+            targets, decoded_output
+        )
+
+        tqdm_dict = {
+            "val-loss": loss_value,
+        }
+        output = OrderedDict(
+            {"loss": loss_value, "progress_bar": tqdm_dict, "log": tqdm_dict,}
+        )
+        output.update(
+            {
+                "wer": total_wer,
+                "cer": total_cer,
+                "num_tokens": num_tokens,
+                "num_chars": num_chars,
+            }
+        )
+        return output
+
+    def validation_epoch_end(self, outputs: List[Dict]) -> Dict[str, Dict[str, Tensor]]:
+
+        # tqdm_dict = {"val_loss": val_loss_mean, "val_acc": val_acc_mean}
+        # result = {
+        #     "progress_bar": tqdm_dict,
+        #     "log": tqdm_dict,
+        #     "val_loss": val_loss_mean,
+        # }
+        return result
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(
@@ -98,6 +161,33 @@ class LitSTTModel(pl.LightningModule):
         parser.add_argument("--train_batch_size", default=20, type=int)
         parser.add_argument("--val_batch_size", default=20, type=int)
         return parser
+
+
+def transcribe_batch(decoder: Decoder, input_len_proportions, inputs, model):
+    input_sizes = input_len_proportions.mul_(int(inputs.size(3))).int()
+    out, output_sizes = model(inputs, input_sizes)
+    probs = F.softmax(out, dim=-1)
+    decoded_output, _ = decoder.decode(probs, output_sizes)
+    return decoded_output, out, output_sizes
+
+
+def calc_loss(
+    out, output_sizes, targets, target_sizes,
+):
+    prob = F.log_softmax(out, -1)
+    ctc_loss = F.ctc_loss(
+        prob.transpose(0, 1),
+        targets,
+        output_sizes,
+        target_sizes,
+        blank=BLANK_INDEX,
+        zero_infinity=True,
+    )
+
+    batch_size = out.size(0)
+    loss = ctc_loss / batch_size  # average the loss by minibatch
+    return loss
+
 
 def _collate_fn(batch):
     def func(p):
@@ -118,7 +208,9 @@ def _collate_fn(batch):
         target = sample[1]
         seq_length = tensor.size(1)
         inputs[x][0].narrow(1, 0, seq_length).copy_(tensor)
-        input_len_proportion[x] = seq_length / float(max_seqlength)
+        input_len_proportion[x] = seq_length / float(
+            max_seqlength
+        )  # TODO(tilo): why is this necessary?
         target_sizes[x] = len(target)
         targets.append(torch.IntTensor(target))
 
@@ -135,16 +227,21 @@ class AudioDataLoader(DataLoader):
         super().__init__(*args, **kwargs)
         self.collate_fn = _collate_fn
 
-if __name__ == "__main__":
 
+def build_dataset(name="debug", files=["dev-clean"]):
     HOME = os.environ["HOME"]
     asr_path = HOME + "/data/asr_data"
     raw_data_path = asr_path + "/ENGLISH/LibriSpeech"
     conf = DataConfig(LIBRI_VOCAB)
     audio_conf = AudioFeaturesConfig()
-    train_samples = build_librispeech_corpus(raw_data_path, "debug", ["dev-clean"],)
+    samples = build_librispeech_corpus(raw_data_path, name, files,)[:32]
+    dataset = CharSTTDataset(samples, conf=conf, audio_conf=audio_conf,)
+    return dataset
 
-    train_dataset = CharSTTDataset(train_samples, conf=conf, audio_conf=audio_conf,)
+
+if __name__ == "__main__":
+
+    train_dataset = build_dataset()
     vocab_size = len(train_dataset.char2idx)
     BLANK_INDEX = train_dataset.char2idx[BLANK_SYMBOL]
     audio_feature_dim = train_dataset.audio_fe.feature_dim
@@ -158,9 +255,5 @@ if __name__ == "__main__":
         )
     )
 
-    trainer = Trainer(
-        logger=False,
-        checkpoint_callback=False,
-        max_epochs=3
-    )
+    trainer = Trainer(logger=False, checkpoint_callback=False, max_epochs=3)
     trainer.fit(litmodel)
