4e85a8802d366f0048da8fa443c7ad7c447d1048
Tilo Himmelsbach <dertilo@gmail.com>
2020-05-27 17:34:15 +0200

    refactored fairseq collater

--- fairseq_stt_collater.py
+++ fairseq_stt_collater.py
@@ -38,6 +38,60 @@ def pad_and_concat_frames(frames):
     return res
 
 
+def collate_target_tokens(samples, sort_order, do_rightshift, eos_index, pad_index):
+    target = fairseq_data_utils.collate_tokens(
+        [s["target"] for s in samples],
+        pad_index,
+        eos_index,
+        left_pad=False,
+        move_eos_to_beginning=do_rightshift,
+    )
+    target = target.index_select(0, sort_order)
+    return target
+
+
+def prepare_targets(samples, sort_order, pad_index, eos_index):
+    ntokens = sum(len(s["target"]) for s in samples)
+    target = collate_target_tokens(samples, sort_order, False, eos_index, pad_index)
+    prev_output_tokens = collate_target_tokens(
+        samples, sort_order, True, eos_index, pad_index
+    )
+
+    target_lengths = torch.LongTensor(
+        [s["target"].size(0) for s in samples]
+    ).index_select(0, sort_order)
+
+    return ntokens, prev_output_tokens, target, target_lengths
+
+
+def collate(samples, pad_index, eos_index):
+    id = torch.LongTensor([s["id"] for s in samples])
+    frames = pad_and_concat_frames([s["source"] for s in samples])
+    frames, frames_lengths, id, sort_order = _sort_frames_by_lengths(
+        frames, id, samples
+    )
+    if samples[0].get("target", None) is not None:
+        ntokens, prev_output_tokens, target, target_lengths = prepare_targets(
+            samples, sort_order, pad_index, eos_index
+        )
+    else:
+        target = None
+        target_lengths = None
+        prev_output_tokens = None
+        ntokens = sum(len(s["source"]) for s in samples)
+    batch = {
+        "id": id,
+        "ntokens": ntokens,
+        "net_input": {"src_tokens": frames, "src_lengths": frames_lengths},
+        "target": target,
+        "target_lengths": target_lengths,
+        "nsentences": len(samples),
+    }
+    if prev_output_tokens is not None:
+        batch["net_input"]["prev_output_tokens"] = prev_output_tokens
+    return batch
+
+
 class Seq2SeqCollater(object):
     """
         Implements collate function mainly for seq2seq tasks
@@ -52,60 +106,14 @@ class Seq2SeqCollater(object):
         label_index=1,
         pad_index=1,
         eos_index=2,
-        move_eos_to_beginning=True,
+        move_eos_to_beginning=None,
     ):
-        self.feature_index = feature_index
-        self.label_index = label_index
         self.pad_index = pad_index
         self.eos_index = eos_index
-        self.move_eos_to_beginning = move_eos_to_beginning
 
     def collate(self, samples: List[Dict]):
         assert len(samples) > 0
         # if len(samples) == 0:
         #     return {}
 
-        id = torch.LongTensor([s["id"] for s in samples])
-        frames = pad_and_concat_frames([s["source"] for s in samples])
-        frames, frames_lengths, id, sort_order = _sort_frames_by_lengths(
-            frames, id, samples
-        )
-
-        target = None
-        target_lengths = None
-        prev_output_tokens = None
-        if samples[0].get("target", None) is not None:
-            ntokens = sum(len(s["target"]) for s in samples)
-            target = fairseq_data_utils.collate_tokens(
-                [s["target"] for s in samples],
-                self.pad_index,
-                self.eos_index,
-                left_pad=False,
-                move_eos_to_beginning=False,
-            )
-            target = target.index_select(0, sort_order)
-            target_lengths = torch.LongTensor(
-                [s["target"].size(0) for s in samples]
-            ).index_select(0, sort_order)
-            prev_output_tokens = fairseq_data_utils.collate_tokens(
-                [s["target"] for s in samples],
-                self.pad_index,
-                self.eos_index,
-                left_pad=False,
-                move_eos_to_beginning=self.move_eos_to_beginning,
-            )
-            prev_output_tokens = prev_output_tokens.index_select(0, sort_order)
-        else:
-            ntokens = sum(len(s["source"]) for s in samples)
-
-        batch = {
-            "id": id,
-            "ntokens": ntokens,
-            "net_input": {"src_tokens": frames, "src_lengths": frames_lengths},
-            "target": target,
-            "target_lengths": target_lengths,
-            "nsentences": len(samples),
-        }
-        if prev_output_tokens is not None:
-            batch["net_input"]["prev_output_tokens"] = prev_output_tokens
-        return batch
+        return collate(samples, self.pad_index, self.eos_index)
