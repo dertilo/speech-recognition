31fcc0496f17d0e5633546f916b74a5dcfc02ae6
Tilo Himmelsbach <dertilo@gmail.com>
2020-10-25 19:27:45 +0100

    spanish_corpora.py

--- data_related/datasets/spanish_corpora.py
+++ data_related/datasets/spanish_corpora.py
@@ -1,10 +1,14 @@
+from typing import Union, List
+
 import wget
 from pathlib import Path
 
 import os
 
 
-def download_spanish_srl_corpora(datasets, download_folder="/tmp"):
+def download_spanish_srl_corpora(
+    datasets: Union[str, List[str]] = "ALL", download_folder="/tmp"
+):
     base_url = "https://www.openslr.org/resources"
 
     name_urls = {
@@ -20,10 +24,10 @@ def download_spanish_srl_corpora(datasets, download_folder="/tmp"):
         for sex in ["male", "female"]
         if not (eid == "74" and sex == "male")  # cause 74 has no male speaker
     }
-    print(name_urls)
-    # name_urls["67_tedx"] = "tedx_spanish_corpus.tgz"
+    name_urls["67_tedx"] = "tedx_spanish_corpus.tgz"
     assert all([k in name_urls.keys() for k in datasets])
 
+    datasets = list(name_urls.keys()) if datasets == "ALL" else datasets
     for data_set in datasets:
         url = name_urls[data_set]
         localfile = os.path.join(download_folder, data_set + Path(url).suffix)
@@ -32,4 +36,4 @@ def download_spanish_srl_corpora(datasets, download_folder="/tmp"):
 
 
 if __name__ == "__main__":
-    download_spanish_srl_corpora("/tmp")
+    download_spanish_srl_corpora(["74_pr_female"], "/tmp")
