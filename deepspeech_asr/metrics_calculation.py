99fcdd8d6fd3320dfba83d1e1a3f1539a14637b9
Tilo Himmelsbach <dertilo@gmail.com>
2020-12-19 11:03:36 +0100

    refactoring metrics_calculation.py

--- deepspeech_asr/metrics_calculation.py
+++ deepspeech_asr/metrics_calculation.py
@@ -1,45 +1,58 @@
 import Levenshtein
-from typing import List
+from typing import List, Tuple
+
+"""
+nemo is using [editdistance](https://github.com/roy-ht/editdistance)
+
+here Levenshtein is used: https://github.com/ztane/python-Levenshtein/
+"""
 
 
-def calc_num_word_errors(s1: str, s2: str) -> int:
+def calc_num_word_errors(hyp: str, ref: str) -> Tuple[int, int]:
+    """
+    based on: https://github.com/SeanNaren/deepspeech.pytorch/blob/master/deepspeech_pytorch/decoder.py
+    https://github.com/SeanNaren/deepspeech.pytorch/blob/78f7fb791f42c44c8a46f10e79adad796399892b/deepspeech_pytorch/decoder.py#L42
+    """
+
     def tokenize(s):
         return s.split()
 
-    b = set(tokenize(s1) + tokenize(s2))
+    b = set(tokenize(hyp) + tokenize(ref))
     token2idx = {t: k for k, t in enumerate(b)}
 
     # map the words to a char array (Levenshtein packages only accepts
     # strings)
-    w1 = [chr(token2idx[w]) for w in s1.split()]
-    w2 = [chr(token2idx[w]) for w in s2.split()]
-    return Levenshtein.distance("".join(w1), "".join(w2))
+    w1 = [chr(token2idx[w]) for w in hyp.split()]
+    w2 = [chr(token2idx[w]) for w in ref.split()]
+
+    len_ref = len(ref.split(" "))
+    return Levenshtein.distance("".join(w1), "".join(w2)), len_ref
 
 
-def calc_num_char_erros(s1: str, s2: str):
-    s1, s2, = (
-        s1.replace(" ", ""),
-        s2.replace(" ", ""),
+def calc_num_char_erros(hyp: str, ref: str) -> Tuple[int, int]:
+    """
+    based on: https://github.com/SeanNaren/deepspeech.pytorch/blob/78f7fb791f42c44c8a46f10e79adad796399892b/deepspeech_pytorch/decoder.py#L62
+    """
+    hyp, ref, = (
+        hyp.replace(" ", ""),
+        ref.replace(" ", ""),
     )  # TODO(tilo): why removing spaces?
-    return Levenshtein.distance(s1, s2)
+
+    len_ref = len(ref.replace(" ", ""))
+    return Levenshtein.distance(hyp, ref), len_ref
 
 
 def calc_wer(hypos: List[str], targets: List[str]):
-    errors_lens = [
-        (calc_num_word_errors(hyp, ref), len(ref.split(" ")))
-        for hyp, ref in zip(hypos, targets)
-    ]
-    num_tokens = sum([l for _, l in errors_lens])
-    errors = sum([s for s, _ in errors_lens])
-    wer = float(errors)/float(num_tokens)
-    return wer
+    errors_lens = [calc_num_word_errors(hyp, ref) for hyp, ref in zip(hypos, targets)]
+    return calc_rate(errors_lens)
+
 
 def calc_cer(hypos: List[str], targets: List[str]):
-    errors_lens = [
-        (calc_num_char_erros(hyp, ref), len(ref.replace(' ','')))
-        for hyp, ref in zip(hypos, targets)
-    ]
+    errors_lens = [calc_num_char_erros(hyp, ref) for hyp, ref in zip(hypos, targets)]
+    return calc_rate(errors_lens)
+
+
+def calc_rate(errors_lens):
     num_tokens = sum([l for _, l in errors_lens])
     errors = sum([s for s, _ in errors_lens])
-    cer = float(errors)/float(num_tokens)
-    return cer
+    return float(errors) / float(num_tokens)
