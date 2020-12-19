5ba0f3b26732d2bf80192dbd432673279191e5f2
Tilo Himmelsbach <dertilo@gmail.com>
2020-12-19 21:50:45 +0100

    black

--- kaldi_tuda_model_server/evaluate_kaldi_tuda.py
+++ kaldi_tuda_model_server/evaluate_kaldi_tuda.py
@@ -9,7 +9,7 @@ from util import data_io
 from data_related.utils import ASRSample
 from deepspeech_asr.metrics_calculation import calc_wer, calc_num_word_errors
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     data_io.read_jsonl("/home/tilo/data/asr_data/GERMAN/results_old_errors.jsonl")
 
@@ -18,13 +18,18 @@ if __name__ == '__main__':
 
     asr_samples = (
         ASRSample(**s)
-        for s in data_io.read_jsonl(f"{base_path}/{folder}/manifest.jsonl.gz",limit=100)
+        for s in data_io.read_jsonl(
+            f"{base_path}/{folder}/manifest.jsonl.gz", limit=100
+        )
     )
-    id2sample:Dict[str,ASRSample] = {s.audio_file:s for s in asr_samples}
+    id2sample = {s.audio_file: s for s in asr_samples}
 
-    hyps_refs = [(d["text"],id2sample[d["id"]].text)
-     for d in data_io.read_jsonl("/home/tilo/data/asr_data/GERMAN/results_old_errors.jsonl")
-                 if "text" in d
-                 ]
-    pprint([(h,r,calc_num_word_errors(h, r)) for h,r in hyps_refs])
+    hyps_refs = [
+        (d["text"], id2sample[d["id"]].text)
+        for d in data_io.read_jsonl(
+            "/home/tilo/data/asr_data/GERMAN/results_old_errors.jsonl"
+        )
+        if "text" in d
+    ]
+    pprint([(h, r, calc_num_word_errors(h, r)) for h, r in hyps_refs])
     print(calc_wer(*zip(*hyps_refs)))
